70beb7373b72c0769455a21ddd934cd4aa57eb4c
Matt Bilbow <hi@mattbilbow.co.uk>
2019-01-03 15:59:15 +0000

    Day 1: Part 2

---
+++ 2018/day1/part2.js
@@ -0,0 +1,30 @@
+const { read } = require("./parser");
+
+const result = (input) => {
+    let list = input.trim().split('\n').map(x => parseInt(x.trim(), 10));
+    let total = 0;
+  
+    let frequency = 0;
+    let i = 0;
+    let duplicate = false;
+    let pastFrequencies = [frequency];
+  
+    while(!duplicate) {
+        // Keep going after list end is reached
+        if (i === list.length) {
+            i = 0;
+        }
+        frequency += list[i];
+        // Set duplicate boolean
+        duplicate = pastFrequencies.includes(frequency);
+        // Add to past frequencies array
+        pastFrequencies.push(frequency);
+        i++;
+    }
+
+    console.log(frequency);
+    return frequency;
+}
+  
+  result(read("./input.txt"));
+
